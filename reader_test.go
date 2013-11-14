32bf2daf3c5fa70b4afb1c4c266f9229f5021221
Anton Egorov <anton.egoroff@gmail.com>
2013-11-14 09:14:26 +0400

    use testify assert for all test

--- reader_test.go
+++ reader_test.go
@@ -3,7 +3,6 @@ package gonx
 import (
 	"github.com/stretchr/testify/assert"
 	"io"
-	"reflect"
 	"strings"
 	"testing"
 )
@@ -11,10 +10,9 @@ import (
 func TestGetFormatRegexp(t *testing.T) {
 	format := "$remote_addr [$time_local] \"$request\""
 	reader := NewReader(strings.NewReader(""), format)
-	expected := `^(?P<remote_addr>[^ ]+) \[(?P<time_local>[^]]+)\] "(?P<request>[^"]+)"$`
-	if re := reader.GetFormatRegexp(); re.String() != expected {
-		t.Errorf("Wrong RE '%v'", re)
-	}
+	assert.Equal(t,
+		reader.GetFormatRegexp().String(),
+		`^(?P<remote_addr>[^ ]+) \[(?P<time_local>[^]]+)\] "(?P<request>[^"]+)"$`)
 }
 
 func TestGetRecord(t *testing.T) {
@@ -27,24 +25,20 @@ func TestGetRecord(t *testing.T) {
 		"request":     "GET /api/foo/bar HTTP/1.1",
 	}
 	rec, err := reader.Read()
-	if err != nil {
-		t.Error(err)
-	}
-	if !reflect.DeepEqual(rec, expected) {
-		t.Errorf("Get invalid record %v", rec)
-	}
-	if _, err := reader.Read(); err != io.EOF {
-		t.Error("End of file expected")
-	}
+	assert.NoError(t, err)
+	assert.Equal(t, rec, expected)
+
+	_, err = reader.Read()
+	assert.Equal(t, err, io.EOF)
 }
 
 func TestInvalidLineFormat(t *testing.T) {
 	format := "$remote_addr [$time_local] \"$request\""
 	file := strings.NewReader(`89.234.89.123 - - [08/Nov/2013:13:39:18 +0000] "GET /api/foo/bar HTTP/1.1"`)
 	reader := NewReader(file, format)
-	if rec, err := reader.Read(); err == nil {
-		t.Errorf("Invalid record error expected, but get the record %+v", rec)
-	}
+	rec, err := reader.Read()
+	assert.Error(t, err)
+	assert.Empty(t, rec)
 }
 
 func TestReadLogFormatFromFile(t *testing.T) {
