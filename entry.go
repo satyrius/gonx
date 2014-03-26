120f7a6b16faaf56a5f9ecaca6d35f96bfa6b0f3
Anton Egorov <anton.egoroff@gmail.com>
2014-03-26 20:50:15 +0400

    entry get float value

--- entry.go
+++ entry.go
@@ -2,6 +2,7 @@ package gonx
 
 import (
 	"fmt"
+	"strconv"
 )
 
 // Parsed log record. Use Get method to retrieve a value by name instead of
@@ -14,6 +15,16 @@ func (entry *Entry) Get(name string) (value string, err error) {
 	value, ok := (*entry)[name]
 	if !ok {
 		err = fmt.Errorf("Field '%v' does not found in record %+v", name, *entry)
+	}
+	return
+}
+
+// Return entry field value as float64. Rutuen nil if field does not exists
+// and convertion error if cannot cast a type.
+func (entry *Entry) GetFloat(name string) (value float64, err error) {
+	tmp, err := entry.Get(name)
+	if err == nil {
+		value, err = strconv.ParseFloat(tmp, 64)
 	}
 	return
 }
--- entry_test.go
+++ entry_test.go
@@ -18,3 +18,22 @@ func TestEntry(t *testing.T) {
 	assert.Error(t, err)
 	assert.Equal(t, val, "")
 }
+
+func TestEntryGetFloat(t *testing.T) {
+	entry := Entry{"foo": "1", "bar": "not a number"}
+
+	// Get existings field
+	val, err := entry.GetFloat("foo")
+	assert.NoError(t, err)
+	assert.Equal(t, val, 1.0)
+
+	// Type casting eror
+	val, err = entry.GetFloat("bar")
+	assert.Error(t, err)
+	assert.Equal(t, val, 0.0)
+
+	// Get field that does not exist
+	val, err = entry.GetFloat("baz")
+	assert.Error(t, err)
+	assert.Equal(t, val, 0.0)
+}
