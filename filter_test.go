1642cdf505064b8f6217173f0f470af3f532f92b
Anton Egorov <anton.egoroff@gmail.com>
2015-09-14 19:45:37 +0300

    Filter start or end only

--- filter_test.go
+++ filter_test.go
@@ -26,6 +26,46 @@ func TestDatetimeFilter(t *testing.T) {
 	assert.Equal(t, filter.Filter(entry), entry)
 }
 
+func TestDatetimeFilterStart(t *testing.T) {
+	// filter contains lower border only
+	filter := &Datetime{
+		Field:  "timestamp",
+		Format: time.RFC3339,
+		Start:  time.Date(2015, time.February, 2, 2, 2, 2, 0, time.UTC),
+	}
+	assert.Implements(t, (*Filter)(nil), filter)
+
+	entry := NewEntry(Fields{
+		"timestamp": "2015-01-01T01:01:01Z",
+	})
+	assert.Nil(t, filter.Filter(entry))
+
+	entry = NewEntry(Fields{
+		"timestamp": "2015-02-02T02:02:02Z",
+	})
+	assert.Equal(t, filter.Filter(entry), entry)
+}
+
+func TestDatetimeFilterEnd(t *testing.T) {
+	// filter contains upper border only
+	filter := &Datetime{
+		Field:  "timestamp",
+		Format: time.RFC3339,
+		End:    time.Date(2015, time.May, 5, 5, 5, 5, 0, time.UTC),
+	}
+	assert.Implements(t, (*Filter)(nil), filter)
+
+	entry := NewEntry(Fields{
+		"timestamp": "2015-01-01T01:01:01Z",
+	})
+	assert.Equal(t, filter.Filter(entry), entry)
+
+	entry = NewEntry(Fields{
+		"timestamp": "2015-05-05T05:05:05Z",
+	})
+	assert.Nil(t, filter.Filter(entry))
+}
+
 func TestDatetimeReduce(t *testing.T) {
 	filter := &Datetime{
 		Field:  "timestamp",
