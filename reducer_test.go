7cf980475bf2f525c33e8131a6a1788c8170108e
Anton Egorov <anton.egoroff@gmail.com>
2015-10-05 15:21:48 +0300

    goconvey test for group reducer

--- reducer_test.go
+++ reducer_test.go
@@ -2,7 +2,6 @@ package gonx
 
 import (
 	. "github.com/smartystreets/goconvey/convey"
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -53,7 +52,7 @@ func TestReducer(t *testing.T) {
 			close(input)
 			total := float64(len(input))
 
-			output := make(chan *Entry, 1) // Make it buffered to avoid deadlock
+			output := make(chan *Entry, 10) // Make it buffered to avoid deadlock
 
 			Convey("Count reducer", func() {
 				reducer := new(Count)
@@ -126,84 +125,57 @@ func TestReducer(t *testing.T) {
 				_, err = result.Field("buz")
 				So(err, ShouldNotBeNil)
 			})
+
+			Convey("Group reducer", func() {
+				reducer := NewGroupBy(
+					// Fields to group by
+					[]string{"host"},
+					// Result reducers
+					&Sum{[]string{"foo", "bar"}},
+					new(Count),
+				)
+				reducer.Reduce(input, output)
+
+				// Collect result entries from output channel to the map, because reading
+				// from channel can be in any order, it depends on each reducer processing
+				resultMap := make(map[string]*Entry)
+				for result := range output {
+					value, err := result.Field("host")
+					So(err, ShouldBeNil)
+					resultMap[value] = result
+				}
+				So(len(resultMap), ShouldEqual, 2)
+
+				// Read and assert first group result
+				result := resultMap["alpha.example.com"]
+
+				floatVal, err := result.FloatField("foo")
+				So(err, ShouldBeNil)
+				So(floatVal, ShouldEqual, 1)
+
+				floatVal, err = result.FloatField("bar")
+				So(err, ShouldBeNil)
+				So(floatVal, ShouldEqual, 2)
+
+				count, err := result.FloatField("count")
+				So(err, ShouldBeNil)
+				So(count, ShouldEqual, 1)
+
+				// Read and assert second group result
+				result = resultMap["beta.example.com"]
+
+				floatVal, err = result.FloatField("foo")
+				So(err, ShouldBeNil)
+				So(floatVal, ShouldEqual, 4+7)
+
+				floatVal, err = result.FloatField("bar")
+				So(err, ShouldBeNil)
+				So(floatVal, ShouldEqual, 5+8)
+
+				count, err = result.FloatField("count")
+				So(err, ShouldBeNil)
+				So(count, ShouldEqual, 2)
+			})
 		})
 	})
-}
-
-func TestGroupByReducer(t *testing.T) {
-	reducer := NewGroupBy(
-		// Fields to group by
-		[]string{"host"},
-		// Result reducers
-		&Sum{[]string{"foo", "bar"}},
-		new(Count),
-	)
-	assert.Implements(t, (*Reducer)(nil), reducer)
-
-	// Prepare import channel
-	input := make(chan *Entry, 10)
-	input <- NewEntry(Fields{
-		"uri":  "/asd/fgh",
-		"host": "alpha.example.com",
-		"foo":  "1",
-		"bar":  "2",
-		"baz":  "3",
-	})
-	input <- NewEntry(Fields{
-		"uri":  "/zxc/vbn",
-		"host": "beta.example.com",
-		"foo":  "4",
-		"bar":  "5",
-		"baz":  "6",
-	})
-	input <- NewEntry(Fields{
-		"uri":  "/ijk/lmn",
-		"host": "beta.example.com",
-		"foo":  "7",
-		"bar":  "8",
-		"baz":  "9",
-	})
-	close(input)
-	output := make(chan *Entry, 2) // Make it buffered to avoid deadlock
-	reducer.Reduce(input, output)
-
-	// Collect result entries from output channel to the map, because reading
-	// from channel can be in any order, it depends on each reducer processing
-	resultMap := make(map[string]*Entry)
-	for result := range output {
-		value, err := result.Field("host")
-		assert.NoError(t, err)
-		resultMap[value] = result
-	}
-	assert.Equal(t, len(resultMap), 2)
-
-	// Read and assert first group result
-	result := resultMap["alpha.example.com"]
-
-	floatVal, err := result.FloatField("foo")
-	assert.NoError(t, err)
-	assert.Equal(t, floatVal, 1.0)
-
-	floatVal, err = result.FloatField("bar")
-	assert.NoError(t, err)
-	assert.Equal(t, floatVal, 2.0)
-
-	value, err := result.Field("count")
-	assert.NoError(t, err)
-	assert.Equal(t, value, "1")
-
-	// Read and assert second group result
-	result = resultMap["beta.example.com"]
-
-	floatVal, err = result.FloatField("foo")
-	assert.NoError(t, err)
-	assert.Equal(t, floatVal, 4.0+7.0)
-
-	floatVal, err = result.FloatField("bar")
-	assert.NoError(t, err)
-	assert.Equal(t, floatVal, 5.0+8.0)
-
-	value, err = result.Field("count")
-	assert.NoError(t, err)
-	assert.Equal(t, value, "2")
 }
